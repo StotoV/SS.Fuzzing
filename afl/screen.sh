1df4e976bdea4ef64ebc40b084397104a5105be9
Tom Stock <info@tomstock.nl>
2020-11-30 13:14:17 +0100

    AFL results and writeup

--- afl/screen.sh
+++ afl/screen.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
+echo core > /proc/sys/kernel/core_pattern
+cd /sys/devices/system/cpu && echo performance | tee cpu*/cpufreq/scaling_governor
+
 NOW=$(date +'%Y-%m-%d_%H:%M:%S')
+SLAVES=${SLAVES:-0}
+OPTIONS=${OPTIONS:-}
+echo "$SLAVES slaves to be created"
 
 mkdir /root/results/$NOW
-afl-fuzz -m none -i /root/testcases -o /root/results/$NOW -- /root/bin/ffmpeg
+echo "Results dir created ($NOW)"
+
+afl-fuzz -M master -m none -i /root/testcases -o /root/results/$NOW -- /root/bin/ffmpeg &
+echo "Master instance running"
+
+i=1
+while [ "$i" -le "$SLAVES" ]; do
+    afl-fuzz -S slave_$i -m none -i /root/testcases -o /root/results/$NOW -- /root/bin/ffmpeg &
+    echo "Slave instance running"
+    i=$(($i + 1))
+done
+
+while true
+do
+	# echo "Press [CTRL+C] to stop.."
+	sleep 1
+done
