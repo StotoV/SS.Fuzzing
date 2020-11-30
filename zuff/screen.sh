62937ed3b87089c077f46a38b4bfeb679dd342ff
Tom Stock <info@tomstock.nl>
2020-11-30 13:38:46 +0100

    Screen edited for zuff

--- zuff/screen.sh
+++ zuff/screen.sh
@@ -2,10 +2,12 @@
 NOW=$(date +'%Y-%m-%d_%H:%M:%S')
 
 mkdir /root/results/$NOW
+FILENAME=$1
 
-while true
+for ((i=0;i<1000;i++))
 do
-	echo "Press [CTRL+C] to stop.."
-	sleep 1
+        x=$[10000 + (RANDOM % 10000)]
+        x=0.0${x:1:2}${x:3:4}
+        zzuf -vq -s $i -r $x < /root/testcases/$FILENAME.mp4 > /root/results/$NOW/$FILENAME${i}_$x.mp4
+        ffmpeg -y -i /root/results/$NOW/$FILENAME${i}_$x.mp4 /root/results/$NOW/$FILENAME${i}_$x.avi 2>> /root/results/$NOW/$FILENAME${i}_$x.txt
 done
-# -i /root/results/small_movie%.mp4 -n 3 /root/testcases/small_movie.mp4
