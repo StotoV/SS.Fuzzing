82107dfe0e3b572dae2b5ce08a0405ef64975839
Tom Stock <info@tomstock.nl>
2020-11-30 14:04:58 +0100

    Radamza now working

--- radamsa/screen.sh
+++ radamsa/screen.sh
@@ -3,12 +3,18 @@ NOW=$(date +'%Y-%m-%d_%H:%M:%S')
 
 mkdir /root/results/$NOW
 mkdir /root/results/$NOW/cases
-FILENAME=$1
 
-for ((i=0;i<=1000;i++))
-do
-        radamsa -o /root/results/$NOW/test$i.mp4 -n 1 /root/testcases/$FILENAME
-        ffmpeg -i /root/results/$NOW/test$i.mp4 /root/results/$NOW/cases/output$i.avi 2>> /root/results/$NOW/cases/output$i.txt
+for file in /root/testcases/*; do 
+    if [ -f "$file" ]; then 
+        FILENAME=$(basename "$file" .mp4)
+
+        for ((i=0;i<=1000;i++))
+        do
+                mkdir /root/results/$NOW/cases/$i
+                radamsa -o /root/results/$NOW/cases/$i/test$i.mp4 -n 1 /root/testcases/$FILENAME.mp4
+                ffmpeg -i /root/results/$NOW/cases/$i/test$i.mp4 /root/results/$NOW/cases/$i/output$i.avi 2>> /root/results/$NOW/cases/$i/output$i.txt
+        done
+    fi 
 done
 
 grep -rnw /root/results/$NOW/cases  -e "moov atom not found" | wc -l 2>> /root/results/$NOW/error.txt
--- zuff/screen.sh
+++ zuff/screen.sh
@@ -2,12 +2,16 @@
 NOW=$(date +'%Y-%m-%d_%H:%M:%S')
 
 mkdir /root/results/$NOW
-FILENAME=$1
 
-for ((i=0;i<1000;i++))
-do
-        x=$[10000 + (RANDOM % 10000)]
-        x=0.0${x:1:2}${x:3:4}
-        zzuf -vq -s $i -r $x < /root/testcases/$FILENAME.mp4 > /root/results/$NOW/$FILENAME${i}_$x.mp4
-        ffmpeg -y -i /root/results/$NOW/$FILENAME${i}_$x.mp4 /root/results/$NOW/$FILENAME${i}_$x.avi 2>> /root/results/$NOW/$FILENAME${i}_$x.txt
+for file in /root/testcases/*; do 
+    if [ -f "$file" ]; then 
+        FILENAME=$(basename "$file" .mp4)
+        for ((i=0;i<1000;i++))
+        do
+                x=$[10000 + (RANDOM % 10000)]
+                x=0.0${x:1:2}${x:3:4}
+                zzuf -vq -s $i -r $x < /root/testcases/$FILENAME.mp4 > /root/results/$NOW/$FILENAME${i}_$x.mp4
+                ffmpeg -y -i /root/results/$NOW/$FILENAME${i}_$x.mp4 /root/results/$NOW/$FILENAME${i}_$x.avi 2>> /root/results/$NOW/$FILENAME${i}_$x.txt
+        done
+    fi 
 done
