16aa67faa7e1a9b6aa01821328df36aca71cc034
Tom Stock <info@tomstock.nl>
2020-11-30 13:35:05 +0100

    Testcase added + radamza run ready

--- radamsa/screen.sh
+++ radamsa/screen.sh
@@ -2,7 +2,14 @@
 NOW=$(date +'%Y-%m-%d_%H:%M:%S')
 
 mkdir /root/results/$NOW
-radamsa -i /root/results/small_movie%.mp4 -n 3 /root/testcases/small_movie.mp4
-ffmpeg -i /root/results/small_movie1.mp4
-ffmpeg -i /root/results/small_movie2.mp4
-ffmpeg -i /root/results/small_movie3.mp4
+mkdir /root/results/$NOW/cases
+FILENAME=$1
+
+for ((i=0;i<=1000;i++))
+do
+        radamsa -o /root/results/$NOW/test$i.mp4 -n 1 /root/testcases/$FILENAME
+        ffmpeg -i /root/results/$NOW/test$i.mp4 /root/results/$NOW/cases/output$i.avi 2>> /root/results/$NOW/cases/output$i.txt
+done
+
+grep -rnw /root/results/$NOW/cases  -e "moov atom not found" | wc -l 2>> /root/results/$NOW/error.txt
+grep -rnw /root/results/$NOW/cases -e "Could not find codec parameters for stream" | wc -l 2>> /root/results/$NOW/warnings.txt
